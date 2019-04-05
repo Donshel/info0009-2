847a72a3aca54a07eece6d13cdceadc1b7bba9cb
François Rozet <francois.rozet@outlook.com>
2019-04-05 19:57:19 +0200

    Affichage des tables

---
+++ www/include/questions/q2d.php
@@ -0,0 +1,15 @@
+<?php
+
+header('Content-Type: application/json');
+
+include('../connect.php');
+
+$file = '../../resources/sql/questions/q2d.sql';
+$sql = file_get_contents($file);
+
+$response = $connect->query($sql);
+$table = $response->fetchAll(PDO::FETCH_NUM);
+
+echo json_encode($table);
+
+?>
