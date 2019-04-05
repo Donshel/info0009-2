aa59f3ae57998b0c9e7b733a55161a9493e0c6a4
François Rozet <francois.rozet@outlook.com>
2019-04-05 22:57:52 +0200

    2.d. Correction

--- www/resources/sql/questions/q2d.sql
+++ www/resources/sql/questions/q2d.sql
@@ -1,28 +1,34 @@
-SELECT nom, prenom, matricule
+SELECT matricule, nom, prenom
 FROM
+(
+	SELECT matricule_participant AS matricule
+	FROM
 	(
-		SELECT T4.matricule AS matricule
+		SELECT DISTINCT T3.matricule AS matricule_auteur, T4.matricule AS matricule_participant
 		FROM
-			(
-				SELECT *
-				FROM
-					(
-						SELECT url, nom_conference, annee_conference
-						FROM articles_conferences
-					) AS T1
-					NATURAL JOIN
-					(
-						SELECT url, matricule_auteur AS matricule
-						FROM articles
-					) AS T2
-			) AS T3
-			LEFT JOIN
-			(
-				SELECT nom_conference, annee_conference, matricule
-				FROM participations
-			) AS T4
-			ON T3.matricule = T4.matricule AND T3.nom_conference = T4.nom_conference AND T3.annee_conference = T4.annee_conference
-			GROUP BY T4.matricule
+		(
+			SELECT * 
+			FROM
+				(
+					SELECT url, nom_conference, annee_conference
+					FROM articles_conferences
+				) AS T1
+				NATURAL JOIN
+				(
+					SELECT url, matricule_auteur AS matricule
+					FROM articles
+				) AS T2
+		) AS T3
+		RIGHT JOIN
+		(
+			SELECT nom_conference, annee_conference, matricule
+			FROM participations
+		) AS T4
+		ON T3.matricule = T4.matricule AND T3.nom_conference = T4.nom_conference AND T3.annee_conference = T4.annee_conference
+		GROUP BY T4.matricule
+		HAVING COUNT( * ) = 1
 	) AS T5
+	WHERE matricule_auteur IS NOT NULL
+) AS T6
 NATURAL JOIN
 auteurs;
