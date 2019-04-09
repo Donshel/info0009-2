7b203ba363c69ec3e14490fa124caaf28cd1227b
François Rozet <francois.rozet@outlook.com>
2019-04-09 23:08:31 +0200

    2.c. Ajout des sujets et seconds auteurs

--- www/include/questions/q2c.php
+++ www/include/questions/q2c.php
@@ -3,26 +3,38 @@
 include('../connect.php');
 
 foreach ($_GET as $key => $value) {
-	$$key = $value;
+	if (!empty($value)) {
+		$$key = $value;
+	}
 }
 
-// Unspecified
+// Spécifications nécessaires
 if (!isset($type)) {
-	echo "Unspecified article type.";
+	echo "Type de l'article non spécifié.";
 	exit;
 } else {
 	if ($type != 'articles_conferences' && $type != 'articles_journaux') {
-		echo "Wrong article type.";
+		echo "Type de l'article incorrect.";
 		exit;
 	}
 }
 
 if ($type == 'articles_conferences') {
 	if (isset($conference)) {
-		$annee_conference = substr($conference, -4);
-		$nom_conference = substr($conference, 0, -5);
+		$i = strrpos($conference, ',');
+		$annee_conference = substr($conference, $i + 1);
+		$nom_conference = substr($conference, 0, $i);
 	} else {
-		echo "Unspecified conference.";
+		echo "Conférence non spécifiée.";
+		exit;
+	}
+} else {
+	if (isset($journal)) {
+		$i = strrpos($journal, ',');
+		$n_journal = substr($journal, $i + 1);
+		$nom_revue = substr($journal, 0, $i);
+	} else {
+		echo "Journal non spécifié.";
 		exit;
 	}
 }
@@ -32,7 +44,7 @@ $desc = $response->fetchAll(PDO::FETCH_NUM);
 
 foreach ($desc as $attr) {
 	if (!isset($$attr[0])) {
-		echo "Unspecified attribute ".$attr[0].".";
+		echo "Attribut ".$attr[0]." non spécifié.";
 		exit;
 	}
 }
@@ -42,31 +54,35 @@ $desc = $response->fetchAll(PDO::FETCH_NUM);
 
 foreach ($desc as $attr) {
 	if (!isset($$attr[0])) {
-		echo "Unspecified attribute ".$attr[0].".";
+		echo "Attribut ".$attr[0]." non spécifié.";
 		exit;
 	}
 }
 
-// Incompatibility
+// Compatibilité et existence
 
 if ($type == 'articles_conferences') {
 	if (substr($date_publication, 0, 4) != $annee_conference) {
-		echo "Incompatibility : date_publication isn't within annee_conference.";
+		echo "Incompatibilité : La date de publication n'est pas dans l'année de la conférence ($annee_conference).";
 		exit;
 	}
 } else {
 	if ($pg_debut > $pg_fin) {
-		echo "Incompatibility : pg_debut is greater than pg_fin.";
-		exit;
-	}
-
-	if ($pg_debut > $pg_fin) {
-		echo "Incompatibility : pg_debut is greater than pg_fin.";
+		echo "Incompatibilité : La page de début est plus grande que celle de fin.";
 		exit;
 	}
 }
 
-// Conference/revue existence
+$sql = "SELECT matricule FROM auteurs WHERE matricule = '$matricule_auteur';";
+
+$response = $connect->query($sql);
+$table = $response->fetchAll(PDO::FETCH_NUM);
+
+if (sizeof($table) == 0) {
+	echo "L'auteur n'existe pas.";
+	exit;
+}
+
 if ($type == 'articles_conferences') {
 	$sql = "SELECT * FROM conferences WHERE nom = '$nom_conference' AND annee = '$annee_conference';";
 
@@ -74,26 +90,42 @@ if ($type == 'articles_conferences') {
 	$table = $response->fetchAll(PDO::FETCH_NUM);
 
 	if (sizeof($table) == 0) {
-		echo "Unexisting conference.";
+		echo "La conférence n'existe pas.";
 		exit;
 	}
 } else {
-	$sql = "SELECT * FROM revues WHERE nom = '$nom_revue';";
+	$sql = "SELECT date_publication FROM articles NATURAL JOIN $type WHERE nom_revue = '$nom_revue' AND n_journal = '$n_journal' LIMIT 1;";
 
 	$response = $connect->query($sql);
 	$table = $response->fetchAll(PDO::FETCH_NUM);
 
 	if (sizeof($table) == 0) {
-		echo "Unexisting revue.";
+		echo "Le journal n'existe pas.";
+		exit;
+	}
+
+	$annee_journal = substr($table[0][0], 0, 4);
+
+	if (substr($date_publication, 0, 4) != $annee_journal) {
+		echo "Incompatibilité : La date de publication n'est pas de la même année que les autres publications du journal ($annee_journal).";
 		exit;
 	}
 }
 
 // LOCK
-$sql = "LOCK TABLES articles WRITE, $type WRITE;";
+$sql = "LOCK TABLES articles WRITE, $type WRITE";
+
+if (isset($sujets_articles)) {
+	$sql .= ", sujets_articles WRITE";
+}
+if (isset($seconds_auteurs)) {
+	$sql .= ", seconds_auteurs WRITE, auteurs READ";
+}
+
+$sql .= ";";
 
 if (!$connect->query($sql)) {
-	echo "Error while locking the tables.";
+	echo "Erreur pendant le verrouillement des tables.";
 	exit;
 }
 
@@ -104,7 +136,7 @@ $response = $connect->query($sql);
 $table = $response->fetchAll(PDO::FETCH_NUM);
 
 if (sizeof($table) != 0) {
-	echo "Already existing article.";
+	echo "Article déjà existant.";
 	$connect->query("ROLLBACK; UNLOCK TABLES;");
 	exit;
 }
@@ -112,16 +144,68 @@ if (sizeof($table) != 0) {
 // INSERT
 $sql = "INSERT INTO articles (url, doi, titre, date_publication, matricule_auteur) VALUES ('$url', $doi, '$titre', '$date_publication', $matricule_auteur);";
 
+if (!$connect->query($sql)) {
+	echo "Erreur pendant l'insertion dans la table 'articles'.";
+	$connect->query("ROLLBACK; UNLOCK TABLES;");
+	exit;
+}
+
 if ($type == 'articles_conferences') {
-	$sql .= "INSERT INTO $type (url, presentation, nom_conference, annee_conference) VALUES ('$url', '$presentation', '$nom_conference', $annee_conference);";
+	$sql = "INSERT INTO $type (url, presentation, nom_conference, annee_conference) VALUES ('$url', '$presentation', '$nom_conference', $annee_conference);";
 } else {
-	$sql .= "INSERT INTO $type (url, pg_debut, pg_fin, nom_revue, n_journal) VALUES ('$url', $pg_debut, $pg_fin, '$nom_revue', $n_journal);";
+	$sql = "INSERT INTO $type (url, pg_debut, pg_fin, nom_revue, n_journal) VALUES ('$url', $pg_debut, $pg_fin, '$nom_revue', $n_journal);";
 }
 
 if (!$connect->query($sql)) {
-	echo "Error while inserting.";
+	echo "Erreur pendant l'insertion dans la table '$type'.";
 	$connect->query("ROLLBACK; UNLOCK TABLES;");
 	exit;
+}
+
+if (isset($sujets_articles)) {
+	$sql = "INSERT INTO sujets_articles (url, sujet) VALUES ";
+
+	$sujets_articles = explode(",", $sujets_articles);
+	foreach ($sujets_articles as &$sujet) {
+		$sujet = trim($sujet);
+		$sql .= "('$url', '$sujet'),";
+	}
+
+	$sql = substr($sql, 0, -1).";";
+
+	if (!$connect->query($sql)) {
+		echo "Error pendant l'insertion dans la table 'sujets_articles'.";
+		$connect->query("ROLLBACK; UNLOCK TABLES;");
+		exit;
+	}
+}
+
+if (isset($seconds_auteurs)) {
+	$sql = "INSERT INTO seconds_auteurs (url, matricule) VALUES ";
+
+	$seconds_auteurs = explode(",", $seconds_auteurs);
+	foreach ($seconds_auteurs as &$matricule) {
+		$matricule = trim($matricule);
+
+		if ($matricule_auteur != $matricule) {
+			$sub_sql = "SELECT matricule FROM auteurs WHERE matricule = $matricule;";
+
+			$response = $connect->query($sub_sql);
+			$table = $response->fetchAll(PDO::FETCH_NUM);
+
+			if (sizeof($table) != 0) {
+				$sql .= "('$url', $matricule),";
+			}
+		}
+	}
+
+	$sql = substr($sql, 0, -1).";";
+
+	if (!$connect->query($sql)) {
+		echo "Erreur pendant l'insertion dans la table 'seconds_auteurs'.";
+		$connect->query("ROLLBACK; UNLOCK TABLES;");
+		exit;
+	}
 }
 
 // COMMIT
