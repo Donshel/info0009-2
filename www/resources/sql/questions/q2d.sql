4bb62cdcd4440f4a4040d20d0ec901fe00a8e228
François Rozet <francois.rozet@outlook.com>
2019-04-09 23:06:12 +0200

    2.d. Correction

--- www/resources/sql/questions/q2d.sql
+++ www/resources/sql/questions/q2d.sql
@@ -1,7 +1,7 @@
 SELECT matricule, nom, prenom
 FROM
 (
-	SELECT matricule_participant AS matricule
+	SELECT matricule_auteur, matricule_participant AS matricule
 	FROM
 	(
 		SELECT DISTINCT T3.matricule AS matricule_auteur, T4.matricule AS matricule_participant
@@ -9,15 +9,15 @@ FROM
 		(
 			SELECT * 
 			FROM
-				(
-					SELECT url, nom_conference, annee_conference
-					FROM articles_conferences
-				) AS T1
-				NATURAL JOIN
-				(
-					SELECT url, matricule_auteur AS matricule
-					FROM articles
-				) AS T2
+			(
+				SELECT url, nom_conference, annee_conference
+				FROM articles_conferences
+			) AS T1
+			NATURAL JOIN
+			(
+				SELECT url, matricule_auteur AS matricule
+				FROM articles
+			) AS T2
 		) AS T3
 		RIGHT JOIN
 		(
@@ -25,10 +25,9 @@ FROM
 			FROM participations
 		) AS T4
 		ON T3.matricule = T4.matricule AND T3.nom_conference = T4.nom_conference AND T3.annee_conference = T4.annee_conference
-		GROUP BY T4.matricule
-		HAVING COUNT( * ) = 1
 	) AS T5
-	WHERE matricule_auteur IS NOT NULL
+	GROUP BY matricule_participant
+	HAVING COUNT(*) = 1 AND matricule_auteur IS NOT NULL
 ) AS T6
 NATURAL JOIN
 auteurs;
