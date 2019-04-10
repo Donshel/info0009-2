ac5a1251b553edb4d0be5757bb038eba4d5b7199
François Rozet <francois.rozet@outlook.com>
2019-04-10 22:11:06 +0200

    2.e. Simplification

--- www/resources/sql/questions/q2e.sql
+++ www/resources/sql/questions/q2e.sql
@@ -1,27 +1,15 @@
-SELECT sujet, count(*) AS popularity
-FROM
+SELECT sujet, COUNT(*) AS popularity
+FROM sujets_articles
+NATURAL JOIN
+articles_conferences
+NATURAL JOIN
 (
-	SELECT sujet
-	FROM
-	(
-		SELECT url
-		FROM
-		(
-			SELECT nom_conference, annee_conference, count(*) AS popularity
-			FROM participations
-			WHERE annee_conference >= 2012
-			GROUP BY nom_conference, annee_conference
-			ORDER BY popularity DESC
-			LIMIT 5
-		) AS T1
-		NATURAL JOIN
-		(
-			SELECT url, nom_conference, annee_conference
-			FROM articles_conferences
-		) AS T2
-	) AS T3
-	NATURAL JOIN
-	sujets_articles
-) AS T4
+	SELECT nom_conference, annee_conference, COUNT(*) AS popularity
+	FROM participations
+	WHERE annee_conference >= 2012
+	GROUP BY nom_conference, annee_conference
+	ORDER BY popularity DESC
+	LIMIT 5
+) AS T1
 GROUP BY sujet
 ORDER BY popularity DESC;
